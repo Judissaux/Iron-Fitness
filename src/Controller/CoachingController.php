5e22fc9aa1599cde418a342777513902e47b894b
justin <justin.dissaux@laposte.net>
2023-03-10 08:12:04 +0000

    Second commit

---
+++ src/Controller/CoachingController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CoachingController extends AbstractController
+{
+    #[Route('/coaching', name: 'app_coaching')]
+    public function index(): Response
+    {
+        return $this->render('autresPages/coaching.html.twig');
+    }
+}
