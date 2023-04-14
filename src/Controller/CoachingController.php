a91695acc14d05cc7467ffae2b351ffd0b7a2edb
justin <justin.dissaux@laposte.net>
2023-04-14 09:36:13 +0000

    Program

--- src/Controller/CoachingController.php
+++ src/Controller/CoachingController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ExerciseRepository;
+use App\Repository\ExercisesRepository;
 use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,9 +12,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class CoachingController extends AbstractController
 {
     #[Route('/coaching', name: 'app_coaching')]
-    public function index(): Response
-    {             
+    public function index(ProgramRepository $programRepo, ExercisesRepository $exercisesRepo): Response
+    {         
+        $programmes = $programRepo->findAll();
         
-        return $this->render('autresPages/coaching.html.twig');
+        $programme = [];
+        foreach($programmes as $name){
+            $programme  [] = [
+                'nameProgram' => $name->getName(),
+                'exercices' => $name->getExercises()->getValues()
+            ];
+        }
+        
+        $exo = [];
+        foreach($programme as $exercice){
+            foreach($exercice['exercices'] as $exos){
+                $exo += $exercisesRepo->findAll($exos->getExercise()->getId());
+            }
+        }        
+
+        return $this->render('autresPages/coaching.html.twig',[
+            'program' => $programme,
+            
+        ]);
     }
 }
